9accad5c4c06b95f6ea2a6d99347c0556f9a8795
Dương Thị Cẩm Tú <camtu28082003@gmail.com>
2025-03-06 10:53:53 +0700

    update

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-document
-  .getElementById("changeColorBtn")
-  .addEventListener("click", function () {
-    // Danh sách màu ngẫu nhiên
-    const colors = [
-      "#ffadad",
-      "#ffd6a5",
-      "#fdffb6",
-      "#caffbf",
-      "#9bf6ff",
-      "#a0c4ff",
-      "#bdb2ff",
-    ];
-
-    // Chọn một màu ngẫu nhiên
-    const randomColor = colors[Math.floor(Math.random() * colors.length)];
-
-    // Thay đổi màu nền
-    document.body.style.backgroundColor = randomColor;
-  });
+document.addEventListener("DOMContentLoaded", () => {
+  alert("Chào mừng bạn đến với trang cá nhân của tôi!");
+});
